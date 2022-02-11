2528874d5bb50847ff4dc231052e5100624510ea
KevinC37 <49222836+KevinC37@users.noreply.github.com>
2022-02-11 15:45:12 +0200

    Title Component
    - Maria added title component

--- src/pages/BookMeeting.jsx
+++ src/pages/BookMeeting.jsx
@@ -2,11 +2,13 @@
 import Timer from '../components/BookMeeting/Timer/Timer';
 import BackgroundContainer from '../components/BookMeeting/BackgroundContainer/Main';
 import ReportIssue from '../components/BookMeeting/ReportIssue/Main';
+import Title from '../components/BookMeeting/Title/Title';
 
 const BookMeeting = () => {
   return (
     <BackgroundContainer>
       <Timer />
+      < Title />
       <ReportIssue />
     </BackgroundContainer>
   );
