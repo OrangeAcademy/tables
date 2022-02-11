89cbc8f38711126f7ff0df0545d2b33b29fb85a3
KevinC37 <49222836+KevinC37@users.noreply.github.com>
2022-02-11 23:04:21 +0200

    added component from other groups

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from "react";
 import { Route, Routes } from 'react-router-dom';
 import BookMeeting from "./pages/BookMeeting";
+import ViewMeeting from "./pages/ViewMeeting";
 
 function App() {
   return (
     <div className="App">
-      <Routes >
+      <Routes>
         <Route path="/" element={<BookMeeting />} exact />
+        <Route path="/view" element={<ViewMeeting />} />
       </Routes>
     </div>
   );
