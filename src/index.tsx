2be6616decdb00f65f685275d6ee3d6f72103041
Aramex86 <slavbez@mail.ru>
2022-03-09 19:20:28 +0200

    merge

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {setupStore} from "./store/store";
-import { BrowserRouter as Router } from 'react-router-dom';
-import {Provider} from "react-redux";
-import App from './App';
-import './global.css';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { setupStore } from "./store/store";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./global.css";
 
 const store = setupStore();
-
-// Redux store
-import {store} from "./redux/store/store";
-import { Provider } from "react-redux";
-
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <App/>
+        <App />
       </Router>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
