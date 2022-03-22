0c7b2c066a99c0151ceb9d7b9dd98997463f42c7
KevinC37 <49222836+KevinC37@users.noreply.github.com>
2022-03-22 17:15:48 +0200

    fixed render issue

--- src/hooks/useDeleteEvent.ts
+++ src/hooks/useDeleteEvent.ts
@@ -1,12 +1,17 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import { getClosestEvent } from 'utils/events.utils';
 import { useAppDispatch } from './redux';
-import {deleteEvent, getEvents} from 'store/Event/actionCreators';
+import { deleteEvent, getEvents } from 'store/Event/actionCreators';
 
 import { useSelector } from 'react-redux';
 import { selectedEventSelector } from 'store/SelectedEvent/selectors';
 import { eventsSelector } from 'store/Event/selectors';
 import { nextEventSelector } from 'store/StateRoom/selectors';
+import {
+  setNextEventStart,
+  storeUpcomingEvent,
+} from 'store/StateRoom/stateRoomSlice';
+import { SERVER_EVENTS_ROUTE } from 'constants/paths';
 
 function useDeleteEvent() {
   const dispatch = useAppDispatch();
@@ -14,13 +19,30 @@ function useDeleteEvent() {
   const events = useSelector(eventsSelector);
   const upcomingEv = useSelector(nextEventSelector);
 
+  const getUpcomingEvent = useCallback(async () => {
+    const events = await (await fetch(SERVER_EVENTS_ROUTE)).json();
+    const nextMeeting = await getClosestEvent(events);
+
+    try {
+      dispatch(storeUpcomingEvent(nextMeeting));
+
+      dispatch(setNextEventStart(nextMeeting.start));
+    } catch (e) {
+      console.log('No events');
+      dispatch(setNextEventStart(''));
+    }
+
+    return nextMeeting;
+  }, [dispatch]);
+
   // delete SELECTED event
   const deleteSelectedEvent = useCallback(async () => {
     if (selectedEvent && selectedEvent.elementId) {
       await dispatch(deleteEvent(selectedEvent.elementId));
-      getClosestEvent(events);
+      await getClosestEvent(events);
+      await getUpcomingEvent();
     }
-  }, [dispatch, events, selectedEvent]);
+  }, [dispatch, events, getUpcomingEvent, selectedEvent]);
 
   // delete (current || upcoming) event
   const deleteUpcomingEvent = useCallback(async () => {
@@ -28,8 +50,9 @@ function useDeleteEvent() {
       await dispatch(deleteEvent(upcomingEv.elementId));
       // DISPATCH FETCH EVENTS
       await dispatch(getEvents());
+      await getUpcomingEvent();
     }
-  }, [dispatch, events, upcomingEv]);
+  }, [dispatch, getUpcomingEvent, upcomingEv]);
 
   return {
     deleteSelectedEvent,
